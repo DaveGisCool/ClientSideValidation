836c97b80ab3943f98dc6ea417c9bf2b863becd1
Dave Gedert <DaveGedert@QuickenLoans.com>
2020-10-06 06:33:48 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ClientSideValidation.Models;
+
+namespace ClientSideValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Result(string FirstName, string LastName, string EmailAddress, string Password)
+        {
+            return Content($"Welcome, {FirstName} {LastName}. You have been emailed at {EmailAddress}. Note your current password is {Password}.");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
